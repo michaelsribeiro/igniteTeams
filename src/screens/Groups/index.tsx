ae11f45448939f9861abe110fffd6d91ad522e1c
Michael Ribeiro <m.ribeiroabd@gmail.com>
2023-10-02 10:56:29 -0300

    Some dependencies

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Groups = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: 20}} >Groups</Text>
+    </View>
+  );
+}
+
+export default Groups;
